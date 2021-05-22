bf8aa4822205abe402a43822e764f040672fe43c
hattmo <matthew@hattmo.com>
2021-05-22 01:09:30 -0400

    add search query in url string

--- docs/search.js
+++ docs/search.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+  const query = new URLSearchParams(window.location.search);
   $("#search-form").on(
     "change keyup paste mouseup search",
     function (event) {
       var value = $(this).val().toLowerCase();
+      if (history.pushState) {
+        query.set("s", value);
+        var newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${query.toString()}`
+        window.history.replaceState({ path: newurl }, "", newurl);
+      }
       $("#link-list a").filter(function () {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
       });
@@ -19,4 +25,10 @@ $(document).ready(function () {
       }
     }
   );
+  const searchQuery = (query.get("s"));
+  if (searchQuery) {
+    $("#search-form").val(searchQuery);
+    $("#search-form").trigger("change")
+  }
+
 });
